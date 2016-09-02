53cd2748797d87f98601a832586c6a8f7775d698
Gabriella Bairan <gbairan@luc.edu>
2016-09-01 19:42:12 -0500

    HelloWorld

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Gabriella Bairan on 9/1/16.
+//  Copyright © 2016 Gabriella Bairan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
